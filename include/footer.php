3836d246e18ee146a87db42f93f4cf27ac2a7ee9
Mohamed Sayed <109354903+MohamedHbo@users.noreply.github.com>
2024-11-30 14:05:39 +0200

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,20 @@
+<section class="col-md-12 col-lg-12 artical-footer">
+		<footer class="footer">
+			<div class="icon">
+				<div class="image-1">
+					<img src="images/facebook-hover.png" alt="">
+				</div>
+				<div class="image-1">
+					<img src="images/instagram-hover.png" alt="">
+				</div>
+				<div class="image-1">
+					<img src="images/twitter-hover.png" alt="">
+				</div>
+			</div>
+		</footer>
+	</section>
+	  <!-- Include all compiled plugins (below), or include individual files as needed -->
+	  <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-rtl.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+	<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+		<a class="navbar-brand" href="#"><b>مدينة حائل</b></a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		<ul class="nav navbar-nav">
+			<li class="active"><a href="index.php"><b>الرئيسية</b> <span class="sr-only">(current)</span></a></li>
+			<li><a href="category.php"><b>المقالات</b></a></li>
+			<li><a href="register.php"><b>تسجيل الدخول</b></a></li>
+			<!-- <li><a href="#">رابط جديد</a></li> -->
+		</ul>
+		
+		<ul class="nav navbar-nav navbar-left">
+			<li class="dropdown">
+			<!-- <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">الإعدادات <span class="caret"></span></a> -->
+			<!-- <ul class="dropdown-menu">
+				<li role="separator" class="divider"></li>
+				<li><a href="#">تسجيل الخروج</a></li>
+			</ul> -->
+			</li>
+		</ul>
+		</div><!-- /.navbar-collapse -->
+	</div><!-- /.container-fluid -->
+	</nav>
